271fdb36f481d571b3d8982d3738b3c005560c2f
basiledayanal <76986813+basiledayanal@users.noreply.github.com>
2021-09-06 09:04:29 +0530

    Update appletprgm1.java

--- CO3/appletprgm1.java
+++ CO3/appletprgm1.java
@@ -1,14 +1,14 @@
 import java.awt.*;
 import java.applet.*;
 public class appletprgm1 extends Applet
-   {
-    public void paint(Graphics g){
-        g.setColor(Color.blue);
-        g.fillOval(100, 0, 100, 100);
-        g.setColor(Color.blue);
-        g.fillRect(100, 300, 300, 100);
-        int xPoints[] = {250,320,170,250};
-        int yPoints[] = {100,150,150,100};
-        g.setColor(Color.blue);
-        g.fillPolygon(xPoints, yPoints, 3);
-    }}
+{
+public void paint(Graphics g){
+g.setColor(Color.blue);
+g.fillOval(100, 0, 100, 100);
+g.setColor(Color.blue);
+g.fillRect(100, 300, 300, 100);
+int xPoints[] = {250,320,170,250};
+int yPoints[] = {100,150,150,100};
+g.setColor(Color.blue);
+g.fillPolygon(xPoints, yPoints, 3);
+}}
