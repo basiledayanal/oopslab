4bfb4e6bb363019e2ee7894d2bdb97c9b7e52100
basiledayanal <76986813+basiledayanal@users.noreply.github.com>
2021-09-23 09:58:39 -0700

    Add files via upload

---
+++ CO4/pg4.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class pg4 
+{
+  public static void main(String[] args) throws Exception 
+  {
+     int i=0;
+
+     FileInputStream file = new FileInputStream("basil.text");
+     FileOutputStream evenFile = new FileOutputStream("even.txt");
+     FileOutputStream oddFile = new FileOutputStream("odd.txt");
+    
+     while((i=file.read())!=-1) 
+     {
+      if(i%2==0)
+      {
+      evenFile.write(i);
+     }
+      else
+      oddFile.write(i);
+     }
+    
+     file.close();
+     evenFile.close();
+     oddFile.close();
+
+      }
+
+    }
